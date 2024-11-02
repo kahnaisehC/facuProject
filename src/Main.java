d0a031a555a9e354fc24e42387a6cbda64e049a3
Ian Hakanson <gabianhak@gmail.com>
2024-11-01 22:33:43 -0300

    feat: add boiler to menu

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Scanner;
+import java.util.function.Function;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Map<String, Runnable> mapOfFunctions = new HashMap<>();
+        mapOfFunctions.put("0", () -> {
+            System.out.println("Elegiste el 0!!!!");
+        });
+        mapOfFunctions.put("1", () -> {
+            System.out.println("Elegiste el 1!!!!");
+        });
+        mapOfFunctions.put("2", () -> {
+            System.out.println("Elegiste el 02!!!!");
+        });
+        mapOfFunctions.put("3", () -> {
+            System.out.println("Elegiste el 0!3!!!");
+        });
+        mapOfFunctions.put("4", () -> {
+            System.out.println("Elegiste el 0!!4!!");
+        });
+        
+        Scanner input = new Scanner(System.in);
 
         String url = "jdbc:postgresql://localhost/test";
         Properties props = new Properties();
@@ -27,9 +49,12 @@ public class Main {
 
         try {
             Connection conn = DriverManager.getConnection(url, props);
-
-
             Statement st = conn.createStatement();
+
+            System.out.println(menu);
+            String option = input.next();
+            mapOfFunctions.get(option).run();
+
             ResultSet rs = st.executeQuery("SELECT * FROM ropas");
             while (rs.next()) {
                 System.out.print("Column 1 returned ");
@@ -38,7 +63,6 @@ public class Main {
             rs.close();
             st.close();
         } catch (Exception e) {
-            System.out.println("Todo funciona!!! (en teoria)");
             throw new RuntimeException(e);
         }
     }
