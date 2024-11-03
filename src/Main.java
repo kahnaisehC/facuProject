f13d873385cd0bb709abd05273fca59e33c3b744
Ian Hakanson <gabianhak@gmail.com>
2024-11-03 00:11:53 -0300

    feat: add more boilerplate to menu

--- src/Main.java
+++ src/Main.java
@@ -10,40 +10,31 @@ import java.util.function.Function;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Torneo{
+    public int id_torneo;
+    public String nombre_torneo;
+    public String equipo_ganador;
+}
 public class Main {
     public static void main(String[] args) {
-        Map<String, Runnable> mapOfFunctions = new HashMap<>();
-        mapOfFunctions.put("0", () -> {
-            System.out.println("Elegiste el 0!!!!");
-        });
-        mapOfFunctions.put("1", () -> {
-            System.out.println("Elegiste el 1!!!!");
-        });
-        mapOfFunctions.put("2", () -> {
-            System.out.println("Elegiste el 02!!!!");
-        });
-        mapOfFunctions.put("3", () -> {
-            System.out.println("Elegiste el 0!3!!!");
-        });
-        mapOfFunctions.put("4", () -> {
-            System.out.println("Elegiste el 0!!4!!");
-        });
         
         Scanner input = new Scanner(System.in);
 
-        String url = "jdbc:postgresql://localhost/test";
+        String url = "jdbc:postgresql://localhost/facuproj";
         Properties props = new Properties();
         props.setProperty("user", "postgres");
         props.setProperty("password", "Welcome4$");
-        String menu =
+        String menuPrincipal =
                         "===========================================\n" +
                         "BIENVENIDO AL GESTOR DE TORNEOS DE FUTBOL!\n" +
                         "===========================================\n" +
                         "Elija una opcion: \n" +
-                        "1.Ingresar un jugador\n" +
-                        "2.Ingresar un equipo\n" +
-                        "3.Crear un partido\n" +
-                        "4.Crear un torneo\n" +
+                        "1.Revisar lista de jugadores\n" +
+                        "2.Revisar lista de equipos\n" +
+                        "3.Revisar lista de partidos\n" +
+                        "4.Revisar lista de torneos\n" +
+                        "5.Crear un torneo\n" +
+                        "6.Crear un partido\n" +
                         "0.Salir\n";
 
 
@@ -51,17 +42,60 @@ public class Main {
             Connection conn = DriverManager.getConnection(url, props);
             Statement st = conn.createStatement();
 
-            System.out.println(menu);
+            System.out.println(menuPrincipal);
             String option = input.next();
-            mapOfFunctions.get(option).run();
+            switch (option){
+                // Revisar lista de jugadores
+                case "1":{
 
-            ResultSet rs = st.executeQuery("SELECT * FROM ropas");
-            while (rs.next()) {
-                System.out.print("Column 1 returned ");
-                System.out.println(rs.getString(2));
+                    break;
+                }
+                // Revisar lista de equipos
+                case "2":{
+
+                    break;
+                }
+                //Revisar lista de partidos
+                case "3":{
+
+                    break;
+                }
+                // Revisar lista de torneos
+                case "4":{
+                    ResultSet torneos = st.executeQuery("SELECT id_torneo, nombre, equipo_ganador FROM torneo");
+                    while(torneos.next()){
+                        Torneo torneo = new Torneo();
+                        torneo.id_torneo = torneos.getInt("id_torneo");
+                        torneo.nombre_torneo = torneos.getString("nombre");
+                        torneo.equipo_ganador = torneos.getString("equipo_ganador");
+                        System.out.printf("%d es el id del torneo\n %s es el nombre del torneo\n %s es el equipo ganador\n", torneo.id_torneo, torneo.nombre_torneo, torneo.equipo_ganador);
+                    }
+                    torneos.close();
+
+                    break;
+                }
+                // Crear un torneo
+                case "5":{
+
+                    break;
+                }
+                // Crear un partido
+                case "6":{
+
+                    break;
+                }
+                // Salir del menu Principal
+                case "0":{
+
+                    break;
+                }
+                // Input no valido
+                default:{
+
+                }
             }
-            rs.close();
             st.close();
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
